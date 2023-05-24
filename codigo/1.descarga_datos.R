570c003285e545c3ffd16e96b65f33e8989fe1d2
Juan Pablo Ruiz Nicolini <juanpabloruiznicolini@gmail.com>
2023-05-23 23:15:24 -0300

    draft descarga masiva a nivel provincial

--- codigo/1.descarga_datos.R
+++ codigo/1.descarga_datos.R
@@ -4,6 +4,8 @@ library(tidyverse) # Easily Install and Load the 'Tidyverse', CRAN v1.3.2
 
 ### DEFINO ELECCIONES A DESCARGAR ----
 
+
+# PRESIDENTE - TOTALES NACIONALES
 (elecciones <- show_available_elections(source = "results") %>% # Fuente de datos Andy Tow (tablas de escrutinios totales)
                 filter(district == "arg", # Nacionales 
                        category == "presi", # Presidente 
@@ -11,20 +13,36 @@ library(tidyverse) # Easily Install and Load the 'Tidyverse', CRAN v1.3.2
  )
 
 
+# PRESIDENTE - POR PROVINCIA
+
+(elecciones_prov <- show_available_elections(source = "results") %>% # Fuente de datos Andy Tow (tablas de escrutinios totales)
+    filter(district != "arg", # Nacionales 
+           category == "presi", # Presidente 
+           round == "gral") # Elecciones Generales
+)
 
 ### DESARGO DATOS ----
 
-
-
+# NIVEL NACIONAL
 datos <- get_multiple_elections(data = elecciones,
                                 source = "results", 
                                 unnest = FALSE)
- 
+ # NIVEL PROVINCIAL
+
+datos_prov <- get_multiple_elections(data = elecciones_prov,
+                                source = "results", 
+                                unnest = FALSE)
 
 ### GENERO BASE DE TRABAJO
 
+# NIVEL NACIONAL
 
 datos %>% 
   unnest(cols = "election") %>% 
   write_csv("entradas/datos.csv")
 
+# NIVEL PROVINCIAL
+
+datos_prov %>% 
+  unnest(cols = "election") %>% 
+  write_csv("entradas/datos_prov.csv")
